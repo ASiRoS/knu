1bb18f4b5fe9a29951bcad38eec3bdf30f5754a8
Artyk Chonbashev <takahiroasiro@gmail.com>
2019-05-21 03:02:50 +0600

    Add link for admin area

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin'], function () {
-    Route::get('/', 'BlankController@index');
+    Route::get('/', 'BlankController@index')->name('home');
     Route::resource('blanks', 'BlankController');
 });
